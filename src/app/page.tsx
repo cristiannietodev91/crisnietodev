f4678ccdf3379f48c6f941fd63039ccc48dc5d4c
Cristian Nieto <cristianandresnieto@gmail.com>
2023-07-08 00:04:48 -0500

    Implement navigation responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Nav from "@/components/nav/Nav";
-import type { NavItem } from "@/components/nav/Nav";
-
-const items: Array<NavItem> = [
-  { href: "/about", text: "About" },
-  { href: "/about", text: "Experience" },
-];
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Nav items={items} />
-    </main>
-  );
+  return <div></div>
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import cx from "classnames";
 import { UrlObject } from "url";
 import NavItem from "./NavItem";
-import style from "./Nav.module.css";
+import styles from "./Nav.module.css";
+import Image from "next/image";
 
 export type NavItem = { text: string; href: string | UrlObject };
 
@@ -10,16 +14,38 @@ type NavProps = {
 };
 
 const Nav = ({ items }: NavProps) => {
+  const [visible, setVisible] = useState(false);
   return (
-    <nav className={style.nav}>
-      <ul className={style.list}>
-        {items.map((item) => (
-          <li key={item.text} className={style.item}>
-            <NavItem href={item.href}>{item.text}</NavItem>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className={cx(styles.container, styles.row)}>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className={styles.vercelLogo}
+        width={100}
+        height={40}
+        priority
+      />
+      <button
+        className={styles.navToggle}
+        aria-label="open navigation"
+        onClick={() => setVisible(!visible)}
+      >
+        <span className={styles.hamburger}></span>
+      </button>
+      <nav
+        className={cx(styles.nav, {
+          [styles.navVisible]: visible,
+        })}
+      >
+        <ul className={styles.list}>
+          {items.map((item) => (
+            <li key={item.text} className={styles.item}>
+              <NavItem href={item.href}>{item.text}</NavItem>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 

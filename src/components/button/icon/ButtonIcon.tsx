65b241294d038da1bc577f589a19d83f783f17b5
Cristian Nieto <cristianandresnieto@gmail.com>
2023-10-17 12:01:03 -0500

    Use Button icon from cs-forms - Add also an skeleton for the header button container

--- src/components/button/icon/ButtonIcon.tsx
+++ src/components/button/icon/ButtonIcon.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react'
 import cx from "classnames";
 import { FaLightbulb, FaRegLightbulb } from "react-icons/fa";
 import styles from "./ButtonIcon.module.css";
+import { Button } from "@cristian.nieto.dev/cs-forms";
 
-type Props = {}
-
-function ButtonIcon({}: Props) {
+function ButtonIcon() {
   const [activeTheme, setActiveTheme] = useState(document.body.dataset.theme || "dark");
   const Icon = activeTheme === "dark" ? FaLightbulb : FaRegLightbulb;
   const inactiveTheme = activeTheme === "dark" ? "light" : "dark";
@@ -16,9 +15,15 @@ function ButtonIcon({}: Props) {
   }, [activeTheme]);
 
   return (
-    <Icon onClick={() => setActiveTheme(inactiveTheme)} className={cx(styles.icon, {
+    <Button variant="plain" onClick={() => setActiveTheme(inactiveTheme)} 
+      size="md" style={{ margin: 0, padding: 0 }}
+      aria-label={`Change to ${inactiveTheme} mode`}
+      title={`Change to ${inactiveTheme} mode`}>
+      <Icon className={cx(styles.icon, {
         [styles.isDark]: activeTheme === "dark",
-    })}/>
+      })} />
+    </Button>
+
   )
 }
 
